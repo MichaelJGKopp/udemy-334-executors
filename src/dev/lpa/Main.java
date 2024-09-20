703459995dfdc5837eaf9789ebb976d9111794cf
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-20 12:08:00 +0200

    add countdown method for colored threads

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,5 +5,19 @@ public class Main {
   public static void main(String[] args) {
 
 
+  private static void countDown() {
+
+    String threadName = Thread.currentThread().getName();
+    var threadColor = ThreadColor.ANSI_RESET;
+    try {
+      threadColor = ThreadColor.valueOf(threadName.toUpperCase());
+    } catch (IllegalArgumentException ignore) {
+      // User may pass a bad color name, Will just ignore this error.
+    }
+
+    String color = threadColor.color();
+    for (int i = 20; i >= 0; i--) {
+      System.out.println(color + " " + threadName.replace("ANSI_", "") + " " + i);
+    }
   }
 }
